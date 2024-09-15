a78b82ef635cc9afced38dff2f486dad48358f9f
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-15 18:47:43 +0530

    Scrapping controller class implemented, logic written for returning scraped data backend to view and download data file as attachment.

---
+++ src/main/java/com/PRODIGY_SD_05/controller/WebScrapingController.java
@@ -0,0 +1,48 @@
+package com.PRODIGY_SD_05.controller;
+
+import com.PRODIGY_SD_05.model.Product;
+import com.PRODIGY_SD_05.service.CsvExportService;
+import com.PRODIGY_SD_05.service.ScrapingService;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/scrap")
+public class WebScrapingController {
+
+    @Autowired
+    private ScrapingService scrapingService;
+    @Autowired
+    private CsvExportService csvExportService;
+
+    @PostMapping("/scrape")
+    public List<Product> scrapeProducts(@RequestParam("url") String url) {
+        return scrapingService.scrapeProductData(url);
+    }
+
+    @PostMapping("/download")
+    public ResponseEntity<InputStreamResource> downloadCsv(@RequestBody List<Product> products) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        csvExportService.writeDataToCsv(products, outputStream);
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        InputStreamResource file = new InputStreamResource(inputStream);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=products.csv")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(outputStream.size())
+                .body(file);
+    }
+}
